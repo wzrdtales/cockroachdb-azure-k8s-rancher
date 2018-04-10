95c9db4698cfd0c38d2e719b74296f4bd66d3d85
Tobias Gurtzick <magic@wizardtales.com>
2018-04-10 13:54:36 +0200

    strip curly braces and do not error out if already unbound
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- Dockerfiles/timefixazure-ntpd/boot.sh
+++ Dockerfiles/timefixazure-ntpd/boot.sh
@@ -1,5 +1,5 @@
 curl -O https://raw.githubusercontent.com/torvalds/linux/master/tools/hv/lsvmbus
-python lsvmbus -vv | grep -w "Time Synchronization" -A 3 | grep "Device_ID" | awk '{print $3}' | tee /root/unbind 
+python lsvmbus -vv | grep -w "Time Synchronization" -A 3 | grep "Device_ID" | awk '{print $3}' | | sed s/[{}]//g | tee /root/unbind || echo "already unbound" 
 rm lsvmbus
 
 ntpd -b -n 

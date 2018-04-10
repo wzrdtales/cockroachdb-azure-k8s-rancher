ac36c8ab3f803d778b36a73685425efdde6a10c5
Tobias Gurtzick <magic@wizardtales.com>
2018-04-10 13:26:41 +0200

    write to unbind
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- Dockerfiles/timefixazure-ntpd/boot.sh
+++ Dockerfiles/timefixazure-ntpd/boot.sh
@@ -1,5 +1,5 @@
 curl -O https://raw.githubusercontent.com/torvalds/linux/master/tools/hv/lsvmbus
-python lsvmbus -vv | grep -w "Time Synchronization" -A 3
+python lsvmbus -vv | grep -w "Time Synchronization" -A 3 | grep "Device_ID" | awk '{print $3}' | tee /root/unbind 
 rm lsvmbus
 
 ntpd -b -n 
